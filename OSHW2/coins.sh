b884a475527465e0046cb498da8f5f728ea5c9ab
CoolRoot <thecoolroot@gmail.com>
2014-01-03 10:37:15 +0200

    sorting files

--- OSHW2/coins.sh
+++ OSHW2/coins.sh
@@ -3,20 +3,17 @@
 #
 function Process
 {
-number=$1
-count25=0
-count10=0
-count5=0
+number=$1; count25=0; count10=0; count5=0
 while [ $number -ge 5 ]; do
-	if   [ $number -ge 25 ]; then # if (n >= 25)
-		let number=number-25 #number = $(expr $number +1)
-		let count25=count25+1
+	if   [ $number -ge 25 ]; then
+		count25=$(expr $number \/ 25 )
+		number=$(expr $number \% 25  )
 	elif [ $number -ge 10 ]; then 
-		let number=number-10
-		let count10=count10+1
-	elif [ $number -ge 5 ]; then
-		let number=number-5
-		let count5=count5+1
+		count10=$(expr $number \/ 10 )
+		number=$(expr $number \% 10  )
+	else
+		count5=$(expr $number \/ 5 )
+		number=$(expr $number \% 5 )
 	fi
 done
 echo -e "We need :\n---------\n$count25 of 25's\n$count10 of 10's\n$count5 of 5's"
--- OSHW2/sort-files.sh
+++ OSHW2/sort-files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #sort-files
 cd files
-exts=$(ls -l |cut -d ' ' -f 9|cut -d'.' -f2|sort|uniq|xargs)
+exts=$(ls -l |awk -F'.' '{print $2}'|sort|uniq|xargs)
 for ext in $exts; do
 	mkdir $ext
 	for i in {1..10}
